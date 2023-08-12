5cb9df52f634e43fa9de7239762dac33e8bada2d
Abhishek Mumar Mishra <mishraabhiak@gmail.com>
2023-08-12 16:33:23 +0530

    changing some thing in code

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -4,6 +4,12 @@ public class Boat implements WaterVehicle{
 
     String name;
     int capacity;
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public String getVehicleName() {
         return name;
