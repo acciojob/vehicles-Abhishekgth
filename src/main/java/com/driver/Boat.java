550884fa8ad9da763f5c3effa74ea6860916c04a
Abhishek Mumar Mishra <mishraabhiak@gmail.com>
2023-08-16 12:22:13 +0530

    change

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -20,6 +20,14 @@ public class Boat implements WaterVehicle{
         return name;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
     @Override
     public int getVehicleCapacity() {
         return capacity;
